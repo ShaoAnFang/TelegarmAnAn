f4db760251d2b8c2a393079881678ac4ba4220db
Clark <ilovet720419@gmail.com>
2019-08-04 15:22:57 +0800

    update

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
-import telebot
 from flask import Flask, request
+import telebot
 
-bot_token = "824978965:AAGYUamuCMH_FupAN_z-axubukiiGB6Gd4g"
-bot = telebot.TeleBot(token = bot_token)
+TOKEN = "824978965:AAGYUamuCMH_FupAN_z-axubukiiGB6Gd4g"
+bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -14,17 +15,20 @@ def start(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
- 
-@server.route('/' + bot_token, methods=['POST'])
+
+
+@server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
+
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url = "https://telegarm-anan.herokuapp.com/" + bot_token)
+    bot.set_webhook(url='https://telegarm-anan.herokuapp.com/' + TOKEN)
     return "!", 200
 
+
 if __name__ == "__main__":
-    server.run(host = "0.0.0.0", port = int(os.environ.get("PORT", 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
